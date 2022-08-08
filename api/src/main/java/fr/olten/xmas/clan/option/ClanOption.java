a1766e46122f2b8225522b771fee3541c252cacb
Azodox_ <likemoi99@gmail.com>
2022-08-08 03:36:11 +0200

    Creating an enum that contains each option of clan

---
+++ api/src/main/java/fr/olten/xmas/clan/option/ClanOption.java
@@ -0,0 +1,14 @@
+package fr.olten.xmas.clan.option;
+
+import lombok.Getter;
+
+public enum ClanOption {
+
+    FRIENDLY_FIRE(Boolean.class);
+
+    private @Getter final Class<?> valueType;
+
+    ClanOption(Class<?> valueType) {
+        this.valueType = valueType;
+    }
+}
