5aaf53e256e7876a829e0aa9978e1afe5a2c86cf
Azodox_ <likemoi99@gmail.com>
2022-08-12 03:14:53 +0200

    Created Bank object that represents a bank in the db

---
+++ api/src/main/java/fr/olten/economy/bank/Bank.java
@@ -0,0 +1,27 @@
+package fr.olten.economy.bank;
+
+import dev.morphia.annotations.Entity;
+import dev.morphia.annotations.Id;
+import lombok.Getter;
+import org.bson.types.ObjectId;
+
+import java.util.Map;
+import java.util.UUID;
+
+@Entity(value = "banks", discriminator = "bank")
+public class Bank {
+
+    @Id
+    private @Getter final ObjectId _id;
+    private @Getter final String name, displayName;
+    private @Getter final double balance;
+    private @Getter final Map<UUID, ObjectId> accounts;
+
+    public Bank(ObjectId _id, String name, String displayName, double balance, Map<UUID, ObjectId> accounts) {
+        this._id = _id;
+        this.name = name;
+        this.displayName = displayName;
+        this.balance = balance;
+        this.accounts = accounts;
+    }
+}
