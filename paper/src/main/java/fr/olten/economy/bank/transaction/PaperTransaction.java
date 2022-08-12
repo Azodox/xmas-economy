ab24be03ded523a13143db7143fbb810895c8007
Azodox_ <likemoi99@gmail.com>
2022-08-13 00:28:46 +0200

    Create PaperTransaction that implements Transaction

---
+++ paper/src/main/java/fr/olten/economy/bank/transaction/PaperTransaction.java
@@ -0,0 +1,27 @@
+package fr.olten.economy.bank.transaction;
+
+import dev.morphia.Datastore;
+import fr.olten.economy.bank.account.BankAccount;
+import fr.olten.economy.bank.account.PaperBankAccountManager;
+
+public class PaperTransaction<S extends BankAccount, R extends BankAccount> implements Transaction<S, R> {
+
+    private final Datastore datastore;
+
+    public PaperTransaction(Datastore datastore) {
+        this.datastore = datastore;
+    }
+
+    @Override
+    public TransactionResult execute(S sender, R receiver, double amount) {
+        var senderManager = new PaperBankAccountManager(datastore, sender);
+        if(!senderManager.decreaseBalanceWouldFail(amount) && senderManager.decreaseBalance(amount).success()){
+            var receiverManager = new PaperBankAccountManager(datastore, receiver);
+            if(!receiverManager.increaseBalanceWouldFail(amount)){
+                receiverManager.increaseBalance(amount);
+                return TransactionResult.SUCCESS;
+            }
+        }
+        return TransactionResult.FAILURE;
+    }
+}
