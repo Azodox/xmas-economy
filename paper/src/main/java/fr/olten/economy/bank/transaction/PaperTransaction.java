6d8d3b7484ce90d7e8a52a8680c6f54f11acf9a3
Azodox <likemoi99@gmail.com>
2022-08-22 02:38:50 +0200

    Put BalanceType in parameter

--- paper/src/main/java/fr/olten/economy/bank/transaction/PaperTransaction.java
+++ paper/src/main/java/fr/olten/economy/bank/transaction/PaperTransaction.java
@@ -1,6 +1,7 @@
 package fr.olten.economy.bank.transaction;
 
 import dev.morphia.Datastore;
+import fr.olten.economy.bank.BalanceType;
 import fr.olten.economy.bank.account.BankAccount;
 import fr.olten.economy.bank.account.PaperBankAccountManager;
 import org.jetbrains.annotations.NotNull;
@@ -16,10 +17,11 @@ public class PaperTransaction<S extends BankAccount, R extends BankAccount> impl
     @Override
     public TransactionResult execute(@NotNull S sender, @NotNull R receiver, double amount) {
         var senderManager = new PaperBankAccountManager(datastore, sender);
-        if(!senderManager.decreaseBalanceWouldFail(amount) && senderManager.decreaseBalance(amount).success()){
+        if(!senderManager.decreaseBalanceWouldFail(BalanceType.FLC, amount) &&
+                senderManager.decreaseBalance(BalanceType.FLC, amount).success()){
             var receiverManager = new PaperBankAccountManager(datastore, receiver);
-            if(!receiverManager.increaseBalanceWouldFail(amount)){
-                receiverManager.increaseBalance(amount);
+            if(!receiverManager.increaseBalanceWouldFail(BalanceType.FLC, amount)){
+                receiverManager.increaseBalance(BalanceType.FLC, amount);
                 return TransactionResult.SUCCESS;
             }
         }
