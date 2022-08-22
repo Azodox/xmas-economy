3b0e53ad8d172851da4581f807ddae96d6178125
Luke <likemoi99@gmail.com>
2022-08-22 16:09:36 +0200

    Created FlcLadder

---
+++ api/src/main/java/fr/olten/economy/ladder/FlcLadder.java
@@ -0,0 +1,7 @@
+package fr.olten.economy.ladder;
+
+import fr.olten.economy.ladder.type.ClanLadder;
+import fr.olten.economy.ladder.type.UUIDLadder;
+
+public interface FlcLadder extends UUIDLadder, ClanLadder {
+}
