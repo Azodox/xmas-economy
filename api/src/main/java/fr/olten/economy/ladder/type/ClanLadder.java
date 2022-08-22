51124cd09156a8d06fbec0c15f998edf2c86641d
Luke <likemoi99@gmail.com>
2022-08-22 16:09:16 +0200

    Created different types of Ladder (UUID and Clan)

---
+++ api/src/main/java/fr/olten/economy/ladder/type/ClanLadder.java
@@ -0,0 +1,8 @@
+package fr.olten.economy.ladder.type;
+
+import fr.olten.economy.clan.Clan;
+
+public interface ClanLadder {
+
+    int getRank(Clan clan);
+}
---
+++ api/src/main/java/fr/olten/economy/ladder/type/UUIDLadder.java
@@ -0,0 +1,8 @@
+package fr.olten.economy.ladder.type;
+
+import java.util.UUID;
+
+public interface UUIDLadder {
+
+    int getRank(UUID uuid);
+}
