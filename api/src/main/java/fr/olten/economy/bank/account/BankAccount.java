d786c485d8bdb914e6f8302969fe4f07620c311a
Azodox <likemoi99@gmail.com>
2022-08-22 02:37:10 +0200

    Rs and flc support in BankAccount

--- api/src/main/java/fr/olten/economy/bank/account/BankAccount.java
+++ api/src/main/java/fr/olten/economy/bank/account/BankAccount.java
@@ -1,6 +1,7 @@
 package fr.olten.economy.bank.account;
 
 import dev.morphia.annotations.*;
+import fr.olten.economy.bank.BalanceType;
 import lombok.Getter;
 import org.bson.types.ObjectId;
 
@@ -16,14 +17,39 @@ public class BankAccount {
     private @Getter final ObjectId _id;
     private @Getter final ObjectId bankId;
     private @Getter final UUID owner;
-    private @Getter final double minBalance, maxBalance, balance;
+    private @Getter final double minFlc, maxFlc, flc, minRs, maxRs, rs;
 
-    public BankAccount(ObjectId _id, ObjectId bankId, UUID owner, double minBalance, double maxBalance, double balance) {
+    public BankAccount(ObjectId _id, ObjectId bankId, UUID owner, double minFlc, double maxFlc, double flc,
+                       double minRs, double maxRs, double rs) {
         this._id = _id;
         this.bankId = bankId;
         this.owner = owner;
-        this.minBalance = minBalance;
-        this.maxBalance = maxBalance;
-        this.balance = balance;
+        this.minFlc = minFlc;
+        this.maxFlc = maxFlc;
+        this.flc = flc;
+        this.minRs = minRs;
+        this.maxRs = maxRs;
+        this.rs = rs;
+    }
+
+    public double getBalance(BalanceType type){
+        return switch (type) {
+            case FLC -> this.getFlc();
+            case RS -> this.getRs();
+        };
+    }
+
+    public double getMinBalance(BalanceType type){
+        return switch (type) {
+            case FLC -> this.getMinFlc();
+            case RS -> this.getMinRs();
+        };
+    }
+
+    public double getMaxBalance(BalanceType type){
+        return switch (type) {
+            case FLC -> this.getMaxFlc();
+            case RS -> this.getMaxRs();
+        };
     }
 }
